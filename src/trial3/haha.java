e91fb56264bdd8ce6fd4d41e73a485ceeeae2505
amudha1 <raovibharao@gmail.com>
2016-02-09 07:24:50 +0530

    this is to update and delete

---
+++ src/trial3/haha.java
@@ -0,0 +1,60 @@
+package trial3;
+import java.sql.*;
+
+public class haha {
+	
+
+	   // JDBC driver name and database URL
+	   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	   static final String DB_URL = "jdbc:mysql://localhost/info";
+
+	   //  Database credentials
+	   static final String USER = "root";
+	   static final String PASS = "";
+	   
+	   public static void main(String[] args) {
+	   Connection conn = null;
+	   Statement stmt = null;
+	   try{
+	      //STEP 2: Register JDBC driver
+	      Class.forName("com.mysql.jdbc.Driver");
+
+	      //STEP 3: Open a connection
+	      System.out.println("Connecting to a selected database...");
+	      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+	      System.out.println("Connected database successfully...");
+	      
+	      //STEP 4: Execute a query
+	      System.out.println("Creating statement...");
+	      stmt = conn.createStatement();
+	      String sql = "DELETE FROM users WHERE username = 'surabhi'" ;
+	      //String sql = "UPDATE users SET username = 'surabhi'  WHERE  username='sush'";          
+	      stmt.executeUpdate(sql);
+
+	      // Now you can extract all the records
+	      // to see the remaining records
+	     
+	   }catch(SQLException se){
+	      //Handle errors for JDBC
+	      se.printStackTrace();
+	   }catch(Exception e){
+	      //Handle errors for Class.forName
+	      e.printStackTrace();
+	   }finally{
+	      //finally block used to close resources
+	      try{
+	         if(stmt!=null)
+	            conn.close();
+	      }catch(SQLException se){
+	      }// do nothing
+	      try{
+	         if(conn!=null)
+	            conn.close();
+	      }catch(SQLException se){
+	         se.printStackTrace();
+	      }//end finally try
+	   }//end try
+	   System.out.println("Goodbye!");
+	}//end main
+	}//end JDBCExample
+
